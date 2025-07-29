1ed80ac800367d4b0777aded63ec8c9747dd4b03
shayanahmad1999 <shayanahmad235@gmail.com>
2025-07-29 16:11:42 +0500

    working on text interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue';
+import TextInterpolation from './components/TextInterpolation.vue';
 </script>
 <template>
   <HelloWorld />
+
+  <TextInterpolation />
 </template>
 <style scoped></style>
---
+++ src/components/TextInterpolation.vue
@@ -0,0 +1,14 @@
+<script setup>
+const myMessage = 'Yellow World';
+const myNumber = 10;
+function add(x, y) {
+    return x + y;
+}
+</script>
+
+<template>
+    <h1>Text Interpolation</h1>
+    <p>Message: {{ myMessage }}</p>
+    <p>Number: {{ myNumber }}</p>
+    <p>Add Two Numbers: {{ add(2, 11) }}</p>
+</template>
