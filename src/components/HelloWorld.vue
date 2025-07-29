275b39298e53ff7a5020f2c7bb13946e924da036
shayanahmad1999 <shayanahmad235@gmail.com>
2025-07-29 16:00:08 +0500

    working on nested component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,20 @@
 <!-- JS -->
 <script setup>
+import NestedComponent from './NestedComponent.vue';
+
 console.log('Hello World from console');
 </script>
 
 <!-- HTML -->
 <template>
     <h1>Hello World</h1>
+    <h2>This is Parent Component</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, maiores facere quis exercitationem at libero,
+        labore
+        provident dolores eius, quam natus earum quo. Voluptates error et consectetur cupiditate perspiciatis veritatis?
+    </p>
+    <NestedComponent />
 </template>
 
 <!-- CSS -->
---
+++ src/components/NestedComponent.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>--Nested Component (Children Component)</h1>
+    <h3>This is some extra content from the children component</h3>
+</template>
