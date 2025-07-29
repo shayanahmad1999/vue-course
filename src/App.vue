ec585d3c89359f208375baf1e23fb0f115935f9b
shayanahmad1999 <shayanahmad235@gmail.com>
2025-07-29 18:26:58 +0500

    working on event handler

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AttritubeBinding from './components/AttritubeBinding.vue';
+import EventHandler from './components/EventHandler.vue';
 import GlobalStyle from './components/GlobalStyle.vue';
 import HelloWorld from './components/HelloWorld.vue';
 import LocalStyle from './components/LocalStyle.vue';
@@ -18,5 +19,7 @@ import TextInterpolation from './components/TextInterpolation.vue';
   <LocalStyle />
 
   <ModuleStyle />
+
+  <EventHandler />
 </template>
 <style scoped></style>
---
+++ src/components/EventHandler.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increment = () => count.value++;
+const decrement = () => count.value--;
+
+const formSubmit = (e) => {
+    e.preventDefault();
+    alert('valid credentials');
+}
+
+const showMessage = (message) => alert(message);
+
+</script>
+
+<template>
+    <h1 class="default">Count: {{ count }}</h1>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+
+    <form @submit="formSubmit">
+        <input type="text" placeholder="Enter your name...">
+        <input type="email" placeholder="Enter your email...">
+        <button type="submit">Submit</button>
+    </form>
+
+    <button @click="showMessage('i am here')">Show Message</button>
+</template>
+
+<style scoped>
+.default {
+    color: black;
+}
+</style>
