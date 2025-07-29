1ad7d4a3cfb4fd1d6818aa5980bba236701df3d4
shayanahmad1999 <shayanahmad235@gmail.com>
2025-07-29 15:54:38 +0500

    working on creating component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
-<script setup></script>
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
 <template>
-  <h1>Hello Vue</h1>
+  <HelloWorld />
 </template>
 <style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<!-- JS -->
+<script setup>
+console.log('Hello World from console');
+</script>
+
+<!-- HTML -->
+<template>
+    <h1>Hello World</h1>
+</template>
+
+<!-- CSS -->
+<style>
+h1 {
+    color: rgb(16, 130, 92);
+}
+</style>
