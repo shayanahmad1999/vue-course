20f60c99d892f8dd5d5fcb586a6e46d096123f6a
shayanahmad1999 <shayanahmad235@gmail.com>
2025-07-30 19:10:13 +0500

    working on name slot

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@ import CustomeValidatorProps from './components/CustomeValidatorProps.vue';
 import EventsParent from './components/EventsParent.vue';
 import SlotComponent from './components/SlotComponent.vue';
 import FallbackContent from './components/FallbackContent.vue';
+import NamedSlot from './components/NamedSlot.vue';
 
 const firstName = ref('Shayan');
 const lastName = ref('Ahmad');
@@ -75,5 +76,16 @@ const lastName = ref('Ahmad');
   <FallbackContent>
     <!-- <p>Fallback will not work because I'm providing content inside the component</p> -->
   </FallbackContent>
+  <hr />
+  <NamedSlot>
+    <!-- v-slot:nameOfSlot -->
+    <template v-slot:one>
+      <h1>This Content will go to slot "one"</h1>
+    </template>
+
+    <template v-slot:two>
+      <h1>This Content will go to slot "two"</h1>
+    </template>
+  </NamedSlot>
 </template>
 <style scoped></style>
---
+++ src/components/NamedSlot.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+    <slot name="one"></slot>
+    <slot name="two"></slot>
+</template>
